651e02b051bc22effd5a000c01fb965989d763ba
Nicolaj Nyvang <nicolajnyvang@gmail.com>
2020-03-11 20:20:26 +0100

    Added a very simple UI to save and display a few fields: Name Address Phone Email

---
+++ src/main/java/dk/nyvang/android/myandroidstorage/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.nyvang.android.myandroidstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
